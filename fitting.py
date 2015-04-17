3bc5284852b3a27754ece2edeb9b77e2cffa903e
ttshimiz <tshimizu@astro.umd.edu>
2015-04-17 00:26:02 -0400

    Create the Filter class that stores all the filters

--- fitting.py
+++ fitting.py
@@ -9,36 +9,15 @@ import numpy as np
 import emcee
 import astropy.constants as c
 import astropy.units as u
-
-# FILTERS
-w1_filter = np.loadtxt('Filters/WISE/RSR-W1.EE.txt')
-w2_filter = np.loadtxt('Filters/WISE/RSR-W2.EE.txt')
-w3_filter = np.loadtxt('Filters/WISE/RSR-W3.EE.txt')
-w4_filter = np.loadtxt('Filters/WISE/RSR-W4.EE.txt')
-h70_filter = np.loadtxt('Filters/Herschel/PACS70.txt')
-h70_ind = (h70_filter[:, 0] > 50) & (h70_filter[:, 0] < 105)
-h70_filter = h70_filter[h70_ind]
-h160_filter = np.loadtxt('Filters/Herschel/PACS160.txt')
-h250_filter = np.loadtxt('Filters/Herschel/PSW.txt')
-h250_ind = (h250_filter[:, 0] > 180) & (h250_filter[:, 0] < 310)
-h250_filter = h250_filter[h250_ind]
-h250_filter = h250_filter[-1::-1]
-h350_filter = np.loadtxt('Filters/Herschel/PMW.txt')
-h350_ind = (h350_filter[:, 0] > 250) & (h350_filter[:, 0] < 450)
-h350_filter = h350_filter[h350_ind]
-h350_filter = h350_filter[-1::-1]
-h500_filter = np.loadtxt('Filters/Herschel/PLW.txt')
-h500_ind = (h500_filter[:, 0] > 360) & (h500_filter[:, 0] < 620)
-h500_filter = h500_filter[h500_ind]
-h500_filter = h500_filter[-1::-1]
+from glob import glob
 
 # CONSTANTS
 c_micron = c.c.to(u.micron/u.s).value
 
 
-def log_like(params, x, y, yerr, sed_model, fixed):
+def log_like(params, x, y, yerr, sed_model, fixed, filts, filt_all):
 
-    model_fluxes = calc_model(x, params, sed_model, fixed)
+    model_fluxes = calc_model(x, params, sed_model, fixed, filts, filt_all)
 
     return -0.5*(np.sum((y-model_fluxes)**2/yerr**2+np.log(2*np.pi*yerr**2)))
 
@@ -61,9 +40,9 @@ def log_prior(params, sed_model, fixed):
     return lp
 
 
-def log_post(params, x, y, yerr, sed_model, fixed):
+def log_post(params, x, y, yerr, sed_model, fixed, filts, filt_all):
 
-    llike = log_like(params, x, y, yerr, sed_model, fixed)
+    llike = log_like(params, x, y, yerr, sed_model, fixed, filts, filt_all)
     lprior = log_prior(params, sed_model, fixed)
 
     if not np.isfinite(lprior) or not np.isfinite(llike):
@@ -71,43 +50,9 @@ def log_post(params, x, y, yerr, sed_model, fixed):
     return lprior + llike
 
 
-def convolve_with_trans(sed_waves, sed_flux, filt_waves, filt_trans):
-    """
-    Function to integrate the SED over a bandpass with
-    a defined transmission curve
-
-    Input Parameters
-    ----------------
-        sed_waves   = wavelengths at which the SED is defined
-        sed_flux    = flux densities of the SED
-        filt_waves  = wavelengths at which the filter is defined
-        filt_trans  = transmission of the filter
-
-    Output
-    ------
-        fnu = integrated flux density
-
-    """
-
-    # Interpolate the transmission to the same wavelengths as the SED
-    # Use a value of 0 for where the filter isn't defined
-    if ~all(sed_waves == filt_waves):
-        interp_trans = np.interp(sed_waves, filt_waves, filt_trans,
-                                 left=0, right=0)
-    else:
-        interp_trans = filt_trans
-
-    # Integrate over all wavelengths (but use the frequencies
-    # since the SED is in Jy)
-    integ_num = np.trapz(c_micron/sed_waves, interp_trans*sed_flux)
-    integ_denom = np.trapz(c_micron/sed_waves, interp_trans)
-
-    return integ_num/integ_denom
-
-
 # Calculate monochromatic flux densities of the model using the
 # filter transmission curves and redshift of the source
-def calc_model(waves, params, sed_model, fixed):
+def calc_model(waves, params, sed_model, fixed, filts, filt_all):
     """
     For each observed wavelength we need to determine which filter to use.
     3.4, 4.6, 12., and 22. micron will be assumed to be WISE filters.
@@ -126,41 +71,17 @@ def calc_model(waves, params, sed_model, fixed):
     # Dummy model
     dummy = sed_model.copy()
 
-    for i, w in enumerate(waves):
-        if w == 3.4:
-            filter = w1_filter
-        elif w == 4.6:
-            filter = w2_filter
-        elif w == 12.:
-            filter = w3_filter
-        elif w == 22.:
-            filter = w4_filter
-        elif w == 70.:
-            filter = h70_filter
-        elif w == 160.:
-            filter = h160_filter
-        elif w == 250.:
-            filter = h250_filter
-        elif w == 350.:
-            filter = h350_filter
-        elif w == 500.:
-            filter = h500_filter
-
-        filter_waves = filter[:, 0]
-        filter_trans = filter[:, 1]
-
-        # Transform to rest frame
-        rest_waves = filter_waves / (1 + sed_model.redshift)
+    for i, f in enumerate(filts):
 
         # Calculate the model at the rest frame wavelengths
         dummy.parameters[~fixed] = params
+        rest_waves = filt_all.filter_waves[f] / (1+sed_model.redshift)
         rest_total_model = dummy(rest_waves)
 
         # Convolve the SED with the transmission curve
-        mono_flux = convolve_with_trans(filter_waves, rest_total_model,
-                                        filter_waves, filter_trans)
-
-        model_fluxes[i] = mono_flux
+        model_fluxes[i] = filt_all.calc_mono_flux(f, rest_waves,
+                                                  rest_total_model,
+                                                  dummy.redshift)
 
     return model_fluxes
 
@@ -186,9 +107,9 @@ class SEDBayesFitter(object):
     def set_nthreads(self, nt):
         self.threads = nt
 
-    def fit(self, model, x, y, yerr):
+    def fit(self, model, x, y, yerr=None, filts=None):
 
-        fixed = np.array([model.fixed.values()])
+        fixed = np.array(model.fixed.values())
         self.ndims = np.sum(~fixed)
 
         # Use the current model parameters as the initial values
@@ -196,9 +117,13 @@ class SEDBayesFitter(object):
         init_walkers = [init + 1e-4*np.random.randn(self.ndims)
                         for k in range(self.nwalkers)]
 
+        # Create the Filter object that stores all of the filters
+        filt_all = Filters()
+
         # Setup the MCMC sampler
         mcmc = emcee.EnsembleSampler(self.nwalkers, self.ndims, log_post,
-                                     args=(x, y, yerr, model, fixed),
+                                     args=(x, y, yerr, model, fixed,
+                                           filts, filt_all),
                                      threads=self.threads)
 
         mcmc.run_mcmc(init_walkers, self.nsteps)
@@ -209,3 +134,46 @@ class SEDBayesFitter(object):
         mcmc.pool.close()
 
         model.parameters[~fixed] = np.median(self.chain_nb, axis=0)
+
+
+class Filters(object):
+
+    def __init__(self):
+
+        fn = glob('./Filters/*.txt')
+        self.names = [x.split('/')[2].split('.')[0] for x in fn]
+        self.filter_trans = {}
+        self.filter_waves = {}
+
+        for i, n in enumerate(fn):
+            data = np.loadtxt(n)
+            fw = data[:, 0]
+            ft = data[:, 1]
+            ind = ft > 3e-3
+            fw = fw[ind]
+            ft = ft[ind]
+            isort = np.argsort(fw)
+            fw = fw[isort]
+            ft = ft[isort]
+            self.filter_trans[self.names[i]] = ft
+            self.filter_waves[self.names[i]] = fw
+
+    def get_waves(self, f):
+        return self.filter_waves[f]
+
+    def get_trans(self, f):
+        return self.filter_trans[f]
+
+    def calc_mono_flux(self, filt, sed_waves, sed, redshift=0.0):
+        ft = self.filter_trans[filt]
+        fw = self.filter_waves[filt]
+
+        rest_waves = fw/(1+redshift)
+
+        interp_sed = np.interp(rest_waves, sed_waves, sed,
+                               left=0, right=0)
+
+        integ_num = np.trapz(ft*interp_sed, x=c_micron/rest_waves)
+        integ_denom = np.trapz(ft, x=c_micron/rest_waves)
+
+        return integ_num/integ_denom
