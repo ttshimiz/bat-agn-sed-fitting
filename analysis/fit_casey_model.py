246724499fa5101465638b5d56bef3e274f2adb5
Thomas Shimizu <tshimizu@ricci.astro.umd.edu>
2015-04-29 11:44:49 -0400

    change script to fit sources with 2 undetected points and fix beta at 1.8

--- analysis/fit_casey_model.py
+++ analysis/fit_casey_model.py
@@ -39,16 +39,17 @@ waves = np.array([12., 22., 70., 160., 250., 350., 500.])
 
 # Uncomment to fit sources with only N undetected points.
 # Change the integer on the right side of '==' to N.
-sed_use = sed[np.sum(np.isnan(sed.values), axis=1) == 1]
+sed_use = sed[np.sum(np.isnan(sed.values), axis=1) == 2]
 
 names_use = sed_use.index
 
-base_model = bat_model.GreybodyPowerlaw(0.0, 25., 2.0, 0.0, 2.0, 50.0)
+base_model = bat_model.GreybodyPowerlaw(0.0, 25., 1.8, 0.0, 2.0, 50.0)
 lev_marq = apy_fit.LevMarLSQFitter()
 bayes = bat_fit.SEDBayesFitter(threads=8)
 
 # Fix parameters
 base_model.wturn.fixed = True
+base_model.beta.fixed = True
 
 for n in names_use:
     print 'Fitting: ', n
@@ -86,7 +87,7 @@ for n in names_use:
                           maxiter=1000)
 
     # model_init.wturn.fixed = False
-    model_init.beta.fixed = False
+    # model_init.beta.fixed = False
     
     model_final = bayes.fit(model_init, waves_use, flux_use, yerr=flux_err_use,
                             filts=filt_detected)
@@ -95,12 +96,12 @@ for n in names_use:
                                 plot_components=True, plot_mono_fluxes=True,
                                 filts=filt_use, plot_fit_spread=True,
                                 name=n, plot_params=True)
-    fig_fit.savefig('casey_bayes_results/wturn_fixed50/sed_plots/' + n +
+    fig_fit.savefig('casey_bayes_results/wturn_fixed50_beta_fixed1_8/sed_plots/' + n +
                     '_casey_bayes_wturn_fixed50_sed_fit.png',
                     bbox_inches='tight')
     plt.close(fig_fit)
     fig_triangle = bat_plot.plot_triangle(model_final)
-    fig_triangle.savefig('casey_bayes_results/wturn_fixed50/triangle_plots/' + n +
+    fig_triangle.savefig('casey_bayes_results/wturn_fixed50_beta_fixed1_8/triangle_plots/' + n +
                          '_casey_bayes_wturn_fixed50_triangle.png',
                          bbox_inches='tight')
     plt.close(fig_triangle)
@@ -111,7 +112,7 @@ for n in names_use:
                 'best_fit_model': model_final,
                 'filters': filt_use,
                 'waves': waves}
-    pickle_file = open('casey_bayes_results/wturn_fixed50/pickles/' + n +
+    pickle_file = open('casey_bayes_results/wturn_fixed50_beta_fixed1_8/pickles/' + n +
                        '_casey_bayes_wturn_fixed50.pickle', 'wb')
     pickle.dump(fit_dict, pickle_file)
     pickle_file.close()
