4c76552e38440271e7dccfcc46f1f330d3d67513
ttshimiz <tshimizu@astro.umd.edu>
2015-07-24 00:57:48 -0400

    use new SPIRE fluxes; force fluxes contaminated by companion to be upper limit; fix beta at 2.0;
    This version used to fit all sources with >= 6 detected points. Fixed
    beta at 2.0. All other parameters free.

--- analysis/fit_casey_model.py
+++ analysis/fit_casey_model.py
@@ -27,6 +27,18 @@ wise_data = pd.read_csv('../../bat-data/bat_wise.csv', index_col=0,
 
 sed = herschel_data.join(wise_data[['W3', 'W3_err', 'W4', 'W4_err']])
 
+# SPIRE fluxes that are seriously contaminated by a companion should be upper limits
+psw_flag = herschel_data['PSW_flag']
+pmw_flag = herschel_data['PMW_flag']
+plw_flag = herschel_data['PLW_flag']
+
+sed['PSW_err'][psw_flag == 'AD'] = sed['PSW'][psw_flag == 'AD']
+sed['PSW'][psw_flag == 'AD'] = np.nan
+sed['PMW_err'][pmw_flag == 'AD'] = sed['PMW'][pmw_flag == 'AD']
+sed['PMW'][pmw_flag == 'AD'] = np.nan
+sed['PLW_err'][plw_flag == 'AD'] = sed['PLW'][plw_flag == 'AD']
+sed['PLW'][plw_flag == 'AD'] = np.nan
+
 # Upload info on BAT AGN for redshift and luminosity distance
 bat_info = pd.read_csv('../../bat-data/bat_info.csv', index_col=0)
 
@@ -37,28 +49,29 @@ waves = np.array([12., 22., 70., 160., 250., 350., 500.])
 # Uncomment to fit sources with detections at all wavelengths
 # sed_use = sed.dropna(how='any')
 
-# Uncomment to fit sources with only N undetected points.
+# Uncomment to fit sources with only N detected points.
 # Change the integer on the right side of '==' to N.
-sed_use = sed[np.sum(np.isnan(sed.values), axis=1) == 2]
+sed_use = sed[np.sum(np.isfinite(sed[filt_use].values), axis=1) >= 6]
 
 names_use = sed_use.index
 
-base_model = bat_model.GreybodyPowerlaw(0.0, 25., 1.8, 0.0, 2.0, 50.0)
+base_model = bat_model.GreybodyPowerlaw(0.0, 25., 2.0, 0.0, 2.0, 50.0)
 lev_marq = apy_fit.LevMarLSQFitter()
 bayes = bat_fit.SEDBayesFitter(threads=8)
 
 # Fix parameters
-base_model.wturn.fixed = True
+#base_model.wturn.fixed = True
 base_model.beta.fixed = True
 
 for n in names_use:
+#for n in names_use[1:2]:
     print 'Fitting: ', n
     src_sed = sed_use.loc[n][filt_use]
-    flux = np.array(src_sed)
+    flux = np.array(src_sed, dtype=np.float)
     flux_detected = np.isfinite(flux)
     flux_use = flux[flux_detected]
     src_err = sed_use.loc[n][filt_err]
-    flux_err = np.array(src_err)
+    flux_err = np.array(src_err, dtype=np.float)
     flux_err_use = flux_err[flux_detected]
     filt_detected = filt_use[flux_detected]
     waves_use = waves[flux_detected]
@@ -80,29 +93,24 @@ for n in names_use:
     pownorm_init = np.log10(src_sed['W3']/model_ml.eval_plaw(12))
     model_ml.pownorm.value = pownorm_init
 
-    model_ml.wturn.fixed = True
-    model_ml.beta.fixed = True
-
     model_init = lev_marq(model_ml, waves, flux, weights=1/flux_err,
                           maxiter=1000)
-
-    # model_init.wturn.fixed = False
-    # model_init.beta.fixed = False
     
     model_final = bayes.fit(model_init, waves_use, flux_use, yerr=flux_err_use,
                             filts=filt_detected)
+
     print 'Plotting the fit: ', n
     fig_fit = bat_plot.plot_fit(waves, flux, model_final, obs_err=flux_err,
                                 plot_components=True, plot_mono_fluxes=True,
                                 filts=filt_use, plot_fit_spread=True,
                                 name=n, plot_params=True)
-    fig_fit.savefig('casey_bayes_results/wturn_fixed50_beta_fixed1_8/sed_plots/' + n +
-                    '_casey_bayes_wturn_fixed50_sed_fit.png',
+    fig_fit.savefig('casey_bayes_results/beta_fixed_2/sed_plots/' + n +
+                    '_casey_bayes_beta_fixed_2_sed_fit.png',
                     bbox_inches='tight')
     plt.close(fig_fit)
     fig_triangle = bat_plot.plot_triangle(model_final)
-    fig_triangle.savefig('casey_bayes_results/wturn_fixed50_beta_fixed1_8/triangle_plots/' + n +
-                         '_casey_bayes_wturn_fixed50_triangle.png',
+    fig_triangle.savefig('casey_bayes_results/beta_fixed_2/triangle_plots/' + n +
+                         '_casey_bayes_beta_fixed_2_triangle.png',
                          bbox_inches='tight')
     plt.close(fig_triangle)
     print 'Saving the fit: ', n
@@ -112,7 +120,7 @@ for n in names_use:
                 'best_fit_model': model_final,
                 'filters': filt_use,
                 'waves': waves}
-    pickle_file = open('casey_bayes_results/wturn_fixed50_beta_fixed1_8/pickles/' + n +
-                       '_casey_bayes_wturn_fixed50.pickle', 'wb')
+    pickle_file = open('casey_bayes_results/beta_fixed_2/pickles/' + n +
+                       '_casey_bayes_beta_fixed_2.pickle', 'wb')
     pickle.dump(fit_dict, pickle_file)
     pickle_file.close()
