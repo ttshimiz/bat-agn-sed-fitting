247f41c5e81cf2809bcc985bbd81fa86c4bcbe0a
ttshimiz <ttshimiz@gmail.com>
2015-08-26 16:14:59 -0400

    latest version for fitting with Casey 2012 model

--- analysis/fit_casey_model.py
+++ analysis/fit_casey_model.py
@@ -52,20 +52,23 @@ waves = np.array([12., 22., 70., 160., 250., 350., 500.])
 # Uncomment to fit sources with only N detected points.
 # Change the integer on the right side of '==' to N.
 
-sed_use = sed[np.sum(np.isfinite(sed[filt_use].values), axis=1) >= 6]
+sed_use = sed[np.sum(np.isfinite(sed[filt_use].values), axis=1) == 4]
 
 names_use = sed_use.index
+print len(names_use)
 
-base_model = bat_model.GreybodyPowerlaw(0.0, 25., 2.0, 0.0, 2.0, 50.0)
+base_model = bat_model.GreybodyPowerlaw(0.0, 25., 2.0, 0.0, 2.0, 45.0)
 lev_marq = apy_fit.LevMarLSQFitter()
 bayes = bat_fit.SEDBayesFitter(threads=8)
 
 # Fix parameters
 #base_model.wturn.fixed = True
-base_model.beta.fixed = True
+#base_model.beta.fixed = True
 
-for n in names_use:
-#for n in names_use[1:2]:
+#for n in names_use:
+for n in names_use[2:]:
+#for n in ['CGCG341-006']:
+    
     print 'Fitting: ', n
     src_sed = sed_use.loc[n][filt_use]
     flux = np.array(src_sed, dtype=np.float)
@@ -83,36 +86,65 @@ for n in names_use:
     model_ml = base_model.copy()
     model_ml.set_redshift(src_z)
     model_ml.set_lumD(src_lumD)
-
+    
+    # Roughly guess initial parameters using the data
+    # Use the slope between W4 and W3 as guess for alpha
     alpha_init = (np.log10(src_sed['W4']/src_sed['W3']) /
                   np.log10(waves[1]/waves[0]))
     model_ml.alpha.value = alpha_init
-
-    mdust_init = np.log10(src_sed['PSW']/model_ml.eval_grey(250))
+    
+    # Use the flux density at 250, 160, 350, or 70 as guess for Mdust
+    if not np.isnan(src_sed['PSW']):
+        mdust_init = np.log10(src_sed['PSW']/model_ml.eval_grey(250.))
+    elif not np.isnan(src_sed['PACS160']):
+        mdust_init = np.log10(src_sed['PACS160']/model_ml.eval_grey(160.))
+    elif not np.isnan(src_sed['PMW']):
+        mdust_init = np.log10(src_sed['PMW']/model_ml.eval_grey(350.))
+    elif not np.isnan(src_sed['PACS70']):
+        mdust_init = np.log10(src_sed['PACS70']/model_ml.eval_grey(70.))
     model_ml.mdust.value = mdust_init
-
+    
+    # Use the W3 flux density as guess for normalization of powerlaw
     pownorm_init = np.log10(src_sed['W3']/model_ml.eval_plaw(12))
     model_ml.pownorm.value = pownorm_init
-
+    
+    # Fix certain parameters for the maximum likelihood estimate
+    model_ml.wturn.fixed = True
+    model_ml.beta.fixed = True
+    model_ml.tdust.fixed = True
+    
+    # Fit the model using Levenberg-Marquardt algorithm to get the best initial guesses
     model_init = lev_marq(model_ml, waves_use, flux_use, weights=1/flux_err_use,
                           maxiter=1000)
     
-    model_final = bayes.fit(model_init, waves, flux, yerr=flux_err, use_nondetect=True
+    # Change back the fixed parameters to what's wanted for Bayesian analysis
+    model_init.wturn.fixed = False
+    model_init.beta.fixed = True
+    model_init.tdust.fixed = False
+    
+    # Fit the model using MCMC
+    model_final = bayes.fit(model_init, waves, flux, yerr=flux_err, use_nondetect=True,
                             filts=filt_use)
+
+    # Plot the best fit along with the data
     print 'Plotting the fit: ', n
     fig_fit = bat_plot.plot_fit(waves, flux, model_final, obs_err=flux_err,
                                 plot_components=True, plot_mono_fluxes=True,
                                 filts=filt_use, plot_fit_spread=True,
                                 name=n, plot_params=True)
-    fig_fit.savefig('casey_bayes_results/beta_fixed_2/sed_plots/' + n +
-                    '_casey_bayes_beta_fixed_2_sed_fit.png',
+    fig_fit.savefig('casey_bayes_results/beta_fixed_2_wturn_gaussianPrior/sed_plots/' + n +
+                    '_casey_bayes_beta_fixed_2_wturn_gaussianPrior_sed_fit.png',
                     bbox_inches='tight')
     plt.close(fig_fit)
+
+    # Plot the marginal posterior distributions for each fitted parameter
     fig_triangle = bat_plot.plot_triangle(model_final)
-    fig_triangle.savefig('casey_bayes_results/beta_fixed_2/triangle_plots/' + n +
-                         '_casey_bayes_beta_fixed_2_triangle.png',
+    fig_triangle.savefig('casey_bayes_results/beta_fixed_2_wturn_gaussianPrior/triangle_plots/' + n +
+                         '_casey_bayes_beta_fixed_2_wturn_gaussianPrior_triangle.png',
                          bbox_inches='tight')
     plt.close(fig_triangle)
+
+    # Save the fitting results in a Python pickle file
     print 'Saving the fit: ', n
     fit_dict = {'name': n,
                 'flux': flux,
@@ -120,7 +152,7 @@ for n in names_use:
                 'best_fit_model': model_final,
                 'filters': filt_use,
                 'waves': waves}
-    pickle_file = open('casey_bayes_results/beta_fixed_2/pickles/' + n +
-                       '_casey_bayes_beta_fixed_2.pickle', 'wb')
+    pickle_file = open('casey_bayes_results/beta_fixed_2_wturn_gaussianPrior/pickles/' + n +
+                       '_casey_bayes_beta_fixed_2_wturn_gaussianPrior.pickle', 'wb')
     pickle.dump(fit_dict, pickle_file)
     pickle_file.close()
