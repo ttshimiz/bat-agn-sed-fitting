dc2d1801f1c57033d7d445fcdeac2828c883e79b
ttshimiz <tshimizu@astro.umd.edu>
2015-09-08 11:10:21 -0400

    bug fix

--- analysis/fit_decompir_model.py
+++ analysis/fit_decompir_model.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 30 15:36:40 2015
 
+Created on Thu Apr 30 15:36:40 2015
 @author: ttshimiz
+
 """
 
 import sys
@@ -27,6 +28,18 @@ wise_data = pd.read_csv('../../bat-data/bat_wise.csv', index_col=0,
 
 sed = herschel_data.join(wise_data[['W3', 'W3_err', 'W4', 'W4_err']])
 
+# SPIRE fluxes that are seriously contaminated by a companion should be upper limits
+psw_flag = herschel_data['PSW_flag']
+pmw_flag = herschel_data['PMW_flag']
+plw_flag = herschel_data['PLW_flag']
+
+sed['PSW_err'][psw_flag == 'AD'] = sed['PSW'][psw_flag == 'AD']
+sed['PSW'][psw_flag == 'AD'] = np.nan
+sed['PMW_err'][pmw_flag == 'AD'] = sed['PMW'][pmw_flag == 'AD']
+sed['PMW'][pmw_flag == 'AD'] = np.nan
+sed['PLW_err'][plw_flag == 'AD'] = sed['PLW'][plw_flag == 'AD']
+sed['PLW'][plw_flag == 'AD'] = np.nan
+
 # Upload info on BAT AGN for redshift and luminosity distance
 bat_info = pd.read_csv('../../bat-data/bat_info.csv', index_col=0)
 
@@ -36,21 +49,22 @@ waves = np.array([12., 22., 70., 160., 250., 350., 500.])
 
 # Uncomment to fit sources with only N undetected points.
 # Change the integer on the right side of '==' to N.
-sed_use = sed[np.sum(np.isnan(sed.values), axis=1) <= 4]
+sed_use = sed[np.sum(np.isfinite(sed[filt_use].values), axis=1) >= 4]
 
 names_use = sed_use.index
+#names_use = ['CGCG493-002']
 
-# Base Dale 2014 model
+# Base Decompir 2014 model
 decompir_model = bat_model.DecompIR()
 
 for n in names_use:
     print 'Fitting: ', n
     src_sed = sed_use.loc[n][filt_use]
-    flux = np.array(src_sed)
+    flux = np.array(src_sed, dtype=np.float)
     flux_detected = np.isfinite(flux)
     flux_use = flux[flux_detected]
     src_err = sed_use.loc[n][filt_err]
-    flux_err = np.array(src_err)
+    flux_err = np.array(src_err, dtype=np.float)
     flux_err_use = flux_err[flux_detected]
     filt_detected = filt_use[flux_detected]
     waves_use = waves[flux_detected]
@@ -62,11 +76,12 @@ for n in names_use:
     decompir_model.set_lumD(src_lumD)
 
     decompir_model.fit(waves_use, flux_use, yerr=flux_err_use, filts=filt_detected)
+    print decompir_model.best_fit['host_name']
     print 'Plotting the fit: ', n
     fig = bat_plot.plot_fit_decompir(waves, flux, decompir_model, obs_err=flux_err,
                                    plot_mono_fluxes=True, filts=filt_use,
                                    name=n, plot_params=True, plot_components=True)
-    fig.savefig('./decompir_results/sed_plots/'+n+'_decompir_best_fit_sed.png',
+    fig.savefig('./decompir_results/sb+arp220/sed_plots/'+n+'_decompir_best_fit_sed.png',
                 bbox_inches='tight')
     plt.close(fig)
 
@@ -77,7 +92,7 @@ for n in names_use:
                 'best_fit_model': decompir_model,
                 'filters': filt_use,
                 'waves': waves}
-    pickle_file = open('./decompir_results/pickles/'+n+'_decompir_best_fit_model.pickle',
+    pickle_file = open('./decompir_results/sb+arp220/pickles/'+n+'_decompir_best_fit_model.pickle',
                        'wb')
     pickle.dump(fit_dict, pickle_file)
     pickle_file.close()
