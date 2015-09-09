f5070219343f7e17601089fe500089b1843d4178
ttshimiz <tshimizu@astro.umd.edu>
2015-09-08 21:11:41 -0400

    first write of fitting script for HRS galaxies with Casey model

--- analysis/fit_casey_model_hrs.py
+++ analysis/fit_casey_model_hrs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 18 16:10:19 2015
+Created on Tue Sept 08 14:42:19 2015
 
 @author: ttshimiz
 """
@@ -22,23 +22,62 @@ import matplotlib.pyplot as plt
 # Upload the WISE and Herschel fluxes for HRS
 hrs_data = pd.read_table('../../spire-catalog-analysis/hrs-data/hrs_photometry.txt', index_col=0)
 hrs_sed = pd.DataFrame({'W3': hrs_data['S12'], 'W4': hrs_data['S22'],
-                        'PACS100': hrs_data['S100']*1000, 'PACS160': hrs_data['S160'],
+                        'PACS100': hrs_data['S100']*1000, 'PACS160': hrs_data['S160']*1000,
                         'PSW': hrs_data['S250'], 'PMW': hrs_data['S350'],
                         'PLW': hrs_data['S500']})
 
 hrs_err = pd.DataFrame({'W3_err': hrs_data['err12'], 'W4_err': hrs_data['err22'],
-                        'PACS100_err': hrs_data['err100']*1000, 'PACS160_err': hrs_data['err160'],
+                        'PACS100_err': hrs_data['err100']*1000, 'PACS160_err': hrs_data['err160']*1000,
                         'PSW_err': hrs_data['err_tot250'], 'PMW_err': hrs_data['err_tot350'],
                         'PLW_err': hrs_data['err_tot500']})
 hrs_flag = hrs_data[['Flag12', 'Flag22', 'Flag100', 'Flag160', 'Flag250', 'Flag350', 'Flag500']]
 
+hrs_err.loc[hrs_flag['Flag12'] == 0, 'W3_err'] = 5./3.*hrs_err.loc[hrs_flag['Flag12'] == 0, 'W3_err']
+hrs_err.loc[hrs_flag['Flag22'] == 0, 'W4_err'] = 5./3.*hrs_err.loc[hrs_flag['Flag22'] == 0, 'W4_err']
+hrs_err.loc[hrs_flag['Flag100'] == 0, 'PACS100_err'] = 5./3.*hrs_sed.loc[hrs_flag['Flag100'] == 0, 'PACS100']
+hrs_err.loc[hrs_flag['Flag160'] == 0, 'PACS160_err'] = 5./3.*hrs_sed.loc[hrs_flag['Flag160'] == 0, 'PACS160']
+hrs_err.loc[hrs_flag['Flag250'] == 0, 'PSW_err'] = 5./3.*hrs_sed.loc[hrs_flag['Flag250'] == 0, 'PSW']
+hrs_err.loc[hrs_flag['Flag350'] == 0, 'PMW_err'] = 5./3.*hrs_sed.loc[hrs_flag['Flag350'] == 0, 'PMW']
+hrs_err.loc[hrs_flag['Flag500'] == 0, 'PLW_err'] = 5./3.*hrs_sed.loc[hrs_flag['Flag500'] == 0, 'PLW']
+
+hrs_sed.loc[hrs_flag['Flag12'] == 0, 'W3'] = np.nan
+hrs_sed.loc[hrs_flag['Flag22'] == 0, 'W4'] = np.nan
+hrs_sed.loc[hrs_flag['Flag100'] == 0, 'PACS100'] = np.nan
+hrs_sed.loc[hrs_flag['Flag160'] == 0, 'PACS160'] = np.nan
+hrs_sed.loc[hrs_flag['Flag250'] == 0, 'PSW'] = np.nan
+hrs_sed.loc[hrs_flag['Flag350'] == 0, 'PMW'] = np.nan
+hrs_sed.loc[hrs_flag['Flag500'] == 0, 'PLW'] = np.nan
+
+# Upper limits in the HRS photometry were all 3 sigma upper limits
+# We need to change the data to a 5-sigma cutoff for all of the photometry
 hrs_flag.loc[hrs_sed['W3']/hrs_err['W3_err'] < 5, 'Flag12'] = 0
+hrs_err.loc[(hrs_sed['W3']/hrs_err['W3_err'] < 5), 'W3_err'] = 5.*hrs_err.loc[(hrs_sed['W3']/hrs_err['W3_err'] < 5) & (hrs_sed['W3'] != 0), 'W3_err']
+hrs_sed.loc[hrs_sed['W3']/hrs_err['W3_err'] < 5, 'W3'] = np.nan
+
 hrs_flag.loc[hrs_sed['W4']/hrs_err['W4_err'] < 5, 'Flag22'] = 0
+hrs_err.loc[(hrs_sed['W4']/hrs_err['W4_err'] < 5), 'W4_err'] = 5.*hrs_err.loc[(hrs_sed['W4']/hrs_err['W4_err'] < 5) & (hrs_sed['W4'] != 0), 'W4_err']
+hrs_sed.loc[hrs_sed['W4']/hrs_err['W4_err'] < 5, 'W4'] = np.nan
+
 hrs_flag.loc[hrs_sed['PACS100']/hrs_err['PACS100_err'] < 5, 'Flag100'] = 0
+hrs_err.loc[(hrs_sed['PACS100']/hrs_err['PACS100_err'] < 5), 'PACS100_err'] = 5.*hrs_err.loc[(hrs_sed['PACS100']/hrs_err['PACS100_err'] < 5) & (hrs_sed['PACS100'] != 0), 'PACS100_err']
+hrs_sed.loc[hrs_sed['PACS100']/hrs_err['PACS100_err'] < 5, 'PACS100'] = np.nan
+
 hrs_flag.loc[hrs_sed['PACS160']/hrs_err['PACS160_err'] < 5, 'Flag160'] = 0
+hrs_err.loc[(hrs_sed['PACS160']/hrs_err['PACS160_err'] < 5), 'PACS160_err'] = 5.*hrs_err.loc[(hrs_sed['PACS160']/hrs_err['PACS160_err'] < 5) & (hrs_sed['PACS160'] != 0), 'PACS160_err']
+hrs_sed.loc[hrs_sed['PACS160']/hrs_err['PACS160_err'] < 5, 'PACS160'] = np.nan
+
 hrs_flag.loc[hrs_sed['PSW']/hrs_err['PSW_err'] < 5, 'Flag250'] = 0
+hrs_err.loc[(hrs_sed['PSW']/hrs_err['PSW_err'] < 5), 'PSW_err'] = 5.*hrs_err.loc[(hrs_sed['PSW']/hrs_err['PSW_err'] < 5) & (hrs_sed['PSW'] != 0), 'PSW_err']
+hrs_sed.loc[hrs_sed['PSW']/hrs_err['PSW_err'] < 5, 'PSW'] = np.nan
+
 hrs_flag.loc[hrs_sed['PMW']/hrs_err['PMW_err'] < 5, 'Flag350'] = 0
+hrs_err.loc[(hrs_sed['PMW']/hrs_err['PMW_err'] < 5), 'PMW_err'] = 5.*hrs_err.loc[(hrs_sed['PMW']/hrs_err['PMW_err'] < 5) & (hrs_sed['PMW'] != 0), 'PMW_err']
+hrs_sed.loc[hrs_sed['PMW']/hrs_err['PMW_err'] < 5, 'PMW'] = np.nan
+
 hrs_flag.loc[hrs_sed['PLW']/hrs_err['PLW_err'] < 5, 'Flag500'] = 0
+hrs_err.loc[(hrs_sed['PLW']/hrs_err['PLW_err'] < 5), 'PLW_err'] = 5.*hrs_err.loc[(hrs_sed['PLW']/hrs_err['PLW_err'] < 5) & (hrs_sed['PLW'] != 0), 'PLW_err']
+hrs_sed.loc[hrs_sed['PLW']/hrs_err['PLW_err'] < 5, 'PLW'] = np.nan
+
 
 hrs_detect = pd.DataFrame({'W3_Detect': (hrs_flag['Flag12'] == 1) | (hrs_flag['Flag12'] == 2),
                            'W4_Detect': (hrs_flag['Flag22'] == 1) | (hrs_flag['Flag22'] == 2),
@@ -50,8 +89,8 @@ hrs_detect = pd.DataFrame({'W3_Detect': (hrs_flag['Flag12'] == 1) | (hrs_flag['F
 
 nbands_detect = hrs_detect.sum(axis=1)
 
-filt_use = np.array(['W3', 'W4', 'PACS100', 'PACS160', 'PSW', 'PMW', 'PLW'])
-filt_err = np.array([s+'_err' for s in filt_use])
+filters = np.array(['W3', 'W4', 'PACS100', 'PACS160', 'PSW', 'PMW', 'PLW'])
+filt_err = np.array([s+'_err' for s in filters])
 waves = np.array([12., 22., 100., 160., 250., 350., 500.])
 
 # Uncomment to fit sources with detections at all wavelengths
@@ -60,10 +99,9 @@ waves = np.array([12., 22., 100., 160., 250., 350., 500.])
 # Uncomment to fit sources with only N detected points.
 # Change the integer on the right side of '==' to N.
 
-sed_use = sed[nbands_detect >= 4]
-
+sed_use = hrs_sed[nbands_detect >= 4]
+err_use = hrs_err[nbands_detect >= 4]
 names_use = sed_use.index
-print len(names_use)
 
 base_model = bat_model.GreybodyPowerlaw(0.0, 25., 2.0, 0.0, 2.0, 45.0)
 lev_marq = apy_fit.LevMarLSQFitter()
@@ -71,49 +109,58 @@ bayes = bat_fit.SEDBayesFitter(threads=8)
 
 # Fix parameters
 #base_model.wturn.fixed = True
-#base_model.beta.fixed = True
+base_model.beta.fixed = True
 
-#for n in names_use:
-for n in names_use[2:]:
-#for n in ['CGCG341-006']:
+for n in names_use:
     
-    print 'Fitting: ', n
-    src_sed = sed_use.loc[n][filt_use]
+    print 'Fitting: HRS ', n
+    src_sed = sed_use.loc[n][filters]/1000.
+    src_err = err_use.loc[n][filt_err]/1000.
+    
     flux = np.array(src_sed, dtype=np.float)
-    flux_detected = np.isfinite(flux)
-    flux_use = flux[flux_detected]
-    src_err = sed_use.loc[n][filt_err]
     flux_err = np.array(src_err, dtype=np.float)
-    flux_err_use = flux_err[flux_detected]
-    filt_detected = filt_use[flux_detected]
-    waves_use = waves[flux_detected]
+    
+    flux_detected = np.isfinite(flux)
+    flux_use = flux_err != 0
+    
+    flux_good = flux[flux_detected & flux_use]
+    flux_err_good = flux_err[flux_detected & flux_use]
+    filts_good = filters[flux_detected & flux_use]
+    waves_good = waves[flux_detected & flux_use]
 	
-    src_z = bat_info.loc[n]['Redshift']
-    src_lumD = bat_info.loc[n]['Dist_[Mpc]']
+    src_lumD = hrs_data.loc[n]['Dist_Mpc']
 
     model_ml = base_model.copy()
-    model_ml.set_redshift(src_z)
     model_ml.set_lumD(src_lumD)
-    
+    model_ml.set_redshift(None)
+      
     # Roughly guess initial parameters using the data
     # Use the slope between W4 and W3 as guess for alpha
-    alpha_init = (np.log10(src_sed['W4']/src_sed['W3']) /
-                  np.log10(waves[1]/waves[0]))
-    model_ml.alpha.value = alpha_init
+    if (np.isfinite(src_sed['W3'])) & (np.isfinite(src_sed['W4'])):
+        alpha_init = (np.log10(src_sed['W4']/src_sed['W3']) /
+                  	  np.log10(waves[1]/waves[0]))
+        model_ml.alpha.value = alpha_init
+    else:
+        model_ml.alpha.value = 1.0
     
-    # Use the flux density at 250, 160, 350, or 70 as guess for Mdust
+    # Use the flux density at 250, 160, 350, or 100 as guess for Mdust
     if not np.isnan(src_sed['PSW']):
         mdust_init = np.log10(src_sed['PSW']/model_ml.eval_grey(250.))
     elif not np.isnan(src_sed['PACS160']):
         mdust_init = np.log10(src_sed['PACS160']/model_ml.eval_grey(160.))
     elif not np.isnan(src_sed['PMW']):
         mdust_init = np.log10(src_sed['PMW']/model_ml.eval_grey(350.))
-    elif not np.isnan(src_sed['PACS70']):
-        mdust_init = np.log10(src_sed['PACS70']/model_ml.eval_grey(70.))
+    elif not np.isnan(src_sed['PACS100']):
+        mdust_init = np.log10(src_sed['PACS100']/model_ml.eval_grey(70.))
     model_ml.mdust.value = mdust_init
     
     # Use the W3 flux density as guess for normalization of powerlaw
-    pownorm_init = np.log10(src_sed['W3']/model_ml.eval_plaw(12))
+    if np.isfinite(src_sed['W3']):
+        pownorm_init = np.log10(src_sed['W3']/model_ml.eval_plaw(12))
+    elif np.isfinite(src_sed['W4']):
+        pownorm_init = np.log10(src_sed['W3']/model_ml.eval_plaw(22))
+    else:
+        pownorm_init = 0.0
     model_ml.pownorm.value = pownorm_init
     
     # Fix certain parameters for the maximum likelihood estimate
@@ -122,7 +169,7 @@ for n in names_use[2:]:
     model_ml.tdust.fixed = True
     
     # Fit the model using Levenberg-Marquardt algorithm to get the best initial guesses
-    model_init = lev_marq(model_ml, waves_use, flux_use, weights=1/flux_err_use,
+    model_init = lev_marq(model_ml, waves_good, flux_good, weights=1/flux_err_good,
                           maxiter=1000)
     
     # Change back the fixed parameters to what's wanted for Bayesian analysis
@@ -131,23 +178,23 @@ for n in names_use[2:]:
     model_init.tdust.fixed = False
     
     # Fit the model using MCMC
-    model_final = bayes.fit(model_init, waves, flux, yerr=flux_err, use_nondetect=True,
-                            filts=filt_use)
+    model_final = bayes.fit(model_init, waves[flux_use], flux[flux_use], yerr=flux_err[flux_use], use_nondetect=True,
+                            filts=filters[flux_use])
 
     # Plot the best fit along with the data
     print 'Plotting the fit: ', n
-    fig_fit = bat_plot.plot_fit(waves, flux, model_final, obs_err=flux_err,
+    fig_fit = bat_plot.plot_fit(waves[flux_use], flux[flux_use], model_final, obs_err=flux_err[flux_use],
                                 plot_components=True, plot_mono_fluxes=True,
-                                filts=filt_use, plot_fit_spread=True,
+                                filts=filters[flux_use], plot_fit_spread=True,
                                 name=n, plot_params=True)
-    fig_fit.savefig('casey_bayes_results/beta_fixed_2_wturn_gaussianPrior/sed_plots/' + n +
+    fig_fit.savefig('casey_bayes_results/hrs_beta_fixed_2_wturn_gaussianPrior/sed_plots/HRS' + str(n) +
                     '_casey_bayes_beta_fixed_2_wturn_gaussianPrior_sed_fit.png',
                     bbox_inches='tight')
     plt.close(fig_fit)
 
     # Plot the marginal posterior distributions for each fitted parameter
     fig_triangle = bat_plot.plot_triangle(model_final)
-    fig_triangle.savefig('casey_bayes_results/beta_fixed_2_wturn_gaussianPrior/triangle_plots/' + n +
+    fig_triangle.savefig('casey_bayes_results/hrs_beta_fixed_2_wturn_gaussianPrior/triangle_plots/HRS' + str(n) +
                          '_casey_bayes_beta_fixed_2_wturn_gaussianPrior_triangle.png',
                          bbox_inches='tight')
     plt.close(fig_triangle)
@@ -158,9 +205,9 @@ for n in names_use[2:]:
                 'flux': flux,
                 'flux_err': flux_err,
                 'best_fit_model': model_final,
-                'filters': filt_use,
+                'filters': filters,
                 'waves': waves}
-    pickle_file = open('casey_bayes_results/beta_fixed_2_wturn_gaussianPrior/pickles/' + n +
+    pickle_file = open('casey_bayes_results/hrs_beta_fixed_2_wturn_gaussianPrior/pickles/HRS' + str(n) +
                        '_casey_bayes_beta_fixed_2_wturn_gaussianPrior.pickle', 'wb')
     pickle.dump(fit_dict, pickle_file)
     pickle_file.close()
